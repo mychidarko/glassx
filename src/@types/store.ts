f08d1cf1d8d41a863cf93389db842544bd7a7ad1
Michael Darko <mickdd22@gmail.com>
2022-04-01 12:42:47 +0000

    :sparkles: add new reducers types

--- src/@types/store.ts
+++ src/@types/store.ts
@@ -1,32 +1,33 @@
-import { Reducer } from "react";
+export type State = Record<string, any>;
+export type Reducer<State, Payload = any> = (
+  prevState: State,
+  action: Payload
+) => State | Promise<State>;
+export type Reducers = Record<string, Reducer<State>>;
+export type Hook =
+  | 'onReady'
+  | 'onSave'
+  | 'onRead'
+  | 'onReducerInvoke'
+  | 'onReset';
 
 export interface Options {
-    state?: State;
-    reducers?: Reducers;
-    modules?: Module[];
-    plugins?: any[];
-    compareState?: Boolean;
-};
+  state?: State;
+  reducers?: Reducers;
+  modules?: Module[];
+  plugins?: any[];
+  compareState?: Boolean;
+}
 
 export interface InternalOptions {
-    defaultState: State;
-    state: State;
-    reducers: Reducers;
-    compareState: Boolean;
+  defaultState: State;
+  state: State;
+  reducers: Reducers;
+  compareState: Boolean;
 }
 
 export interface Module {
-    namespace?: string;
-    state?: State;
-    reducers?: Reducers;
-};
-
-export interface State {
-    [key: string]: any;
-};
-
-export interface Reducers {
-    [key: string]: Reducer<State, any>;
-};
-
-export type Hook = "onReady" | "onSave" | "onRead" | "onReducerInvoke" | "onReset";
+  namespace?: string;
+  state?: State;
+  reducers?: Reducers;
+}
