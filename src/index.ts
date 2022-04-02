28ffd9e048a991c5d8d4df4f316e22e3eef62930
Michael Darko <mickdd22@gmail.com>
2022-04-02 17:34:51 +0000

    :sparkles: add exports for types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import GlassX from "./store";
+import GlassX from './store';
 
-export { useStore, useReducer, setStore } from "./store";
-
-export { default as PersistedState } from "./plugins/persist";
+export { useStore, useReducer, setStore } from './store';
+export { default as PersistedState } from './plugins/persist';
+export { Reducer } from './@types/functions';
+export { Plugin, Hook } from './@types/plugin';
+export { State, Module } from './@types/store';
 
 export default GlassX;
