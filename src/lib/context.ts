2d68a8e746ea2d8961c6adbc582b5020eb7646b0
Michael Darko <mickdd22@gmail.com>
2022-04-03 22:22:14 +0000

    :sparkles: add context

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,1 +1,14 @@
-import { Context, createContext } from 'react';
+import { HooksUnsupportedError } from './../utils/error';
+import { TrueContext } from '../@types/core';
+import { createContext } from 'react';
+import Engine from './engine';
+
+const Context = (): TrueContext<Engine> | null => {
+  if (typeof createContext === 'function') {
+    return createContext(new Engine) as TrueContext<Engine>;
+  }
+
+  throw new HooksUnsupportedError();
+};
+
+export default Context();
