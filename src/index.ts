9de6543453f1ef5c9498e662997e0188f262c0e8
Michael Darko <mickdd22@gmail.com>
2022-04-04 10:34:25 +0000

    :bug: fix import error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import GlassX from './store';
+import GlassX from './lib/core';
 
-export { useStore, useReducer, setStore } from './store';
+export { useStore, useReducer, setStore } from './lib/hooks';
 export { default as PersistedState } from './plugins/persist';
 export { Reducer } from './@types/functions';
 export { Plugin, Hook } from './@types/plugin';
-export { State, Module } from './@types/store';
+export { State, Module } from './@types/core';
 
 export default GlassX;
