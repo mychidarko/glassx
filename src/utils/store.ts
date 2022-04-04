5b9b3c5b30966e579f34d23441e9c01a40654a5a
Michael Darko <mickdd22@gmail.com>
2022-04-04 14:57:37 +0000

    :sparkles: add getEngine method

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,3 +1,15 @@
+import { Context } from "lib/context";
+import Engine from "lib/engine";
+
 /// author: Charles Stover - ReactN
 export const copyObject = <Shape>(obj: Shape): Shape =>
   Object.assign(Object.create(null), obj);
+
+/// author: Charles Stover - ReactN
+export function getEngine(): Engine {
+  return (
+    (Context() &&
+      ((Context()._currentValue2) ||
+        (Context()._currentValue)))
+  );
+}
