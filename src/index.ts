e0205166de8b0c7de9f58493ed77b108a4f93936
Michael Darko <mickdd22@gmail.com>
2022-04-05 11:19:10 +0000

    :sparkles: rewrote exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import GlassX from './lib/core';
 
+export * from './@types';
 export { useStore, useReducer, setStore } from './lib/hooks';
 export { default as PersistedState } from './plugins/persist';
-export { Reducer } from './@types/functions';
-export { Plugin, Hook } from './@types/plugin';
-export { State, Module } from './@types/core';
 
 export default GlassX;
