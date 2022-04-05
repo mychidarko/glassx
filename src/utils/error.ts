804f67e4a74765809fc48137ebc2821dd7fb945a
Michael Darko <mickdd22@gmail.com>
2022-04-05 20:59:27 +0000

    :fire: remove unused files

--- src/utils/error.ts
+++
@@ -1,9 +0,0 @@
-export class HooksUnsupportedError extends Error {
-  constructor() {
-    super();
-    this.message =
-      'The installed version of React does not support Context. ' +
-      'Upgrade to React v16.3.0 or later.';
-    this.name = 'ReactHooksUnsupportedError';
-  }
-}
