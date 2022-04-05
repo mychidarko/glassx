cbfbc0d061fd019e7acbcf67ebf7e968363b70b4
Michael Darko <mickdd22@gmail.com>
2022-04-05 11:20:45 +0000

    :sparkles: update core engine

--- src/lib/engine.ts
+++ src/lib/engine.ts
@@ -1,6 +1,8 @@
 import { Hook, Plugin } from '../@types/plugin';
 import { Reducer, Reducers } from '../@types/functions';
 import { InternalOptions, Module, Options, State } from '../@types/core';
+import { Context } from './context';
+import { objectGetListener } from 'utils/store';
 
 export default class Engine {
   private plugins: Plugin[] = [];
@@ -10,6 +12,7 @@ export default class Engine {
     reducers: {},
     compareState: false
   };
+  private propertyListeners = new Map();
 
   public forceUpdate = null;
 
@@ -93,5 +96,125 @@ export default class Engine {
     });
   }
 
+  /**
+   * Set a new value into state
+   * @param {Object} state value to set into state
+   */
+  public set(state: State) {
+    const globalState: State = this._options.state;
 
+    state = {
+      ...globalState,
+      ...state
+    };
+
+    this.applyPluginHook('onSave', state);
+
+    if (this._options.compareState && this.compareState(state)) {
+      return;
+    }
+
+    this._options.state = state;
+  }
+
+  public get(state: string | null = null) {
+    const globalState: State = this._options.state;
+
+    if (!state) {
+      return globalState;
+    }
+
+    this.applyPluginHook('onRead', state);
+
+    const parts = state.split('.');
+    let selectedState = globalState[parts[0]];
+
+    if (parts.length > 1) {
+      selectedState = selectedState[parts[1]];
+    }
+
+    return selectedState;
+  }
+
+  /// author: Charles Stover - ReactN
+  // Map component instance to a state property.
+  public addPropertyListener(property: any, propertyListener: any): void {
+    // If property listeners already exist for this property,
+    //   add this one to the set.
+    if (this.propertyListeners.has(property)) {
+      this.propertyListeners.get(property).add(propertyListener);
+    }
+
+    // If property listeners don't already exist for this property,
+    //   create a set of property listeners that includes this one.
+    else {
+      this.propertyListeners.set(property, new Set([propertyListener]));
+    }
+  }
+
+  /// author: Charles Stover - ReactN
+  // Unmap a component instance from all state properties.
+  public removePropertyListener(propertyListener: any): boolean {
+    let removed = false;
+
+    // Remove this property listener from the global state.
+    for (const propertyListeners of this.propertyListeners.values() as any) {
+      if (propertyListeners.delete(propertyListener)) {
+        removed = true;
+      }
+    }
+
+    return removed;
+  }
+
+  /// author: Charles Stover - ReactN
+  public spyState(propertyListener: () => void) {
+    // When this._options.state is read, execute the listener.
+    return objectGetListener(this._options.state, (property: any) => {
+      this.addPropertyListener(property, propertyListener);
+    });
+  }
+
+  public reducer(reducerName: string): Reducer<State> {
+    const parts = reducerName.split('.');
+    let base: any = this._options.reducers[parts[0]];
+
+    if (parts.length > 1) {
+      base = base[parts[1]];
+    }
+
+    return base;
+  }
+
+  public reset() {
+    this.applyPluginHook('onReset', this._options.defaultState);
+    this.set(this._options.defaultState);
+  }
+
+  public useReducer(reducer: string | Reducer<State>) {
+    if (typeof reducer === 'string') {
+      return this.runner(this.reducer(reducer));
+    }
+
+    this._options.reducers[reducer.name] = reducer;
+    return this.runner(this.reducer(reducer.name));
+  }
+
+  public compareState(state: State | string): boolean {
+    state = JSON.stringify(state);
+    const globalState = JSON.stringify(this._options.state);
+
+    return state === globalState;
+  }
+
+  protected runner(reducer: Reducer<State>) {
+    return async (payload: any) => {
+      const state = reducer(this.get(), payload);
+      this.set(await state);
+    };
+  }
+}
+
+export function getEngine(): Engine {
+  return Context() && (Context()._currentValue2 || Context()._currentValue);
 }
