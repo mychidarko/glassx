ff595544744afe520502c9b450c15fccbee4190b
Michael Darko <mickdd22@gmail.com>
2022-04-05 20:57:00 +0000

    :sparkle: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import GlassX from './lib/core';
-
 export * from './@types';
-export { useStore, useReducer, setStore } from './lib/hooks';
+
+export { useStore, setStore, useReducer } from './lib/hooks';
 export { default as PersistedState } from './plugins/persist';
 
 export default GlassX;
