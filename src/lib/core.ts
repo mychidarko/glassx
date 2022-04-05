94190518cb48ed488533f2b642f601df6ee4bf10
Michael Darko <mickdd22@gmail.com>
2022-04-05 11:20:28 +0000

    🏷️ update core types

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,8 +1,8 @@
+import { SetStateAction } from 'react';
 import { Hook, Plugin } from '../@types/plugin';
-import { Reducer, Reducers, SetStoreFn, UseStoreFn } from '../@types/functions';
-import { InternalOptions, Module, Options, State } from '../@types/core';
-import { getEngine } from 'utils/store';
-import Engine from './engine';
+import { Reducer } from '../@types/functions';
+import { Options, State } from '../@types/core';
+import Engine, { getEngine } from './engine';
 
 export default class GlassX {
   protected static engine: Engine;
@@ -39,59 +39,36 @@ export default class GlassX {
    * Set a new value into state
    * @param {Object} state value to set into state
    */
-  public static set = (state: State) => {
-    const globalState: State = getGlobal();
+  public static set<StateType extends State = State>(
+    state: SetStateAction<StateType>
+  ) {
+    let finalState: State = {};
 
-    const parts = state.split('.');
+    if (typeof state === 'function') {
+      const globalState: State = this.get();
+      const callableState = state as (prevState: State) => StateType;
 
-    if (parts.length > 1) {
-      let selectedState = globalState[parts[0]];
-      selectedState = selectedState[parts[1]];
-
-      state = {
-        ...globalState,
-        [parts[0]]: {
-          ...selectedState,
-          ...state
-        }
+      finalState = {
+        ...finalState,
+        ...callableState(globalState)
       };
     } else {
-      state = {
-        ...globalState,
+      finalState = {
+        ...finalState,
         ...state
       };
     }
 
-    this.applyPluginHook('onSave', state);
-
-    if (this._options.compareState && this.compareState(state)) {
-      return;
-    }
-
-    setGlobal(state);
-  };
+    this.engine.set(finalState);
+  }
 
   /**
    * Get a value from state
    * @param {(string | null)} state key of value to retrieve from state. Returns entire state if `state` is null
    */
   public static get(state: string | null = null) {
-    const globalState: State = getGlobal();
-
-    if (!state) {
-      return globalState;
-    }
-
-    this.applyPluginHook('onRead', state);
-
-    const parts = state.split('.');
-    let selectedState = globalState[parts[0]];
-
-    if (parts.length > 1) {
-      selectedState = selectedState[parts[1]];
-    }
-
-    return selectedState;
+    this.engineInit();
+    return this.engine.get(state);
   }
 
   /**
@@ -99,36 +76,16 @@ export default class GlassX {
    * @param {string} reducerName The reducer to return
    */
   public static reducer(reducerName: string): Reducer<State> {
-    const parts = reducerName.split('.');
-    let base: any = this._options.reducers[parts[0]];
-
-    if (parts.length > 1) {
-      base = base[parts[1]];
-    }
-
-    return base;
+    this.engineInit();
+    return this.engine.reducer(reducerName);
   }
 
   /**
    * Reset state to it's default value
    */
   public static reset() {
-    this.applyPluginHook('onReset', this._options.defaultState);
-    this.set(this._options.defaultState);
-  }
-
-  protected static compareState(state: State | string) {
-    state = JSON.stringify(state);
-    const globalState = JSON.stringify(getGlobal());
-
-    return state === globalState;
-  }
-
-  protected static runner(reducer: Reducer<State>) {
-    return async (payload: any) => {
-      const state = reducer(this.get(), payload);
-      this.set(await state);
-    };
+    this.engineInit();
+    this.engine.reset();
   }
 
   /**
@@ -136,11 +93,7 @@ export default class GlassX {
    * @param {string|Function} reducer The reducer to call
    */
   public static useReducer(reducer: string | Reducer<State>) {
-    if (typeof reducer === 'string') {
-      return this.runner(this.reducer(reducer));
-    }
-
-    this._options.reducers[reducer.name] = reducer;
-    return this.runner(this.reducer(reducer.name));
+    this.engineInit();
+    return this.engine.useReducer(reducer);
   }
 }
