07f67ee9506b3363bfb2c3f5c2e953daecd46280
Michael Darko <mickdd22@gmail.com>
2022-04-05 11:21:05 +0000

    :sparkles: update utils

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,15 +1,25 @@
-import { Context } from "lib/context";
-import Engine from "lib/engine";
-
 /// author: Charles Stover - ReactN
 export const copyObject = <Shape>(obj: Shape): Shape =>
   Object.assign(Object.create(null), obj);
 
 /// author: Charles Stover - ReactN
-export function getEngine(): Engine {
-  return (
-    (Context() &&
-      ((Context()._currentValue2) ||
-        (Context()._currentValue)))
+// Return an object that executes a read listener.
+export function objectGetListener<Shape>(
+  obj: Shape,
+  listener: Function
+): Partial<Shape> {
+  return (Object.keys(obj as any) as (keyof Shape)[]).reduce(
+    (accumulator: Partial<Shape>, key: keyof Shape): Partial<Shape> => {
+      Object.defineProperty(accumulator, key, {
+        configurable: false,
+        enumerable: true,
+        get: (): Shape[keyof Shape] => {
+          listener(key);
+          return obj[key];
+        }
+      });
+      return accumulator;
+    },
+    Object.create(null)
   );
 }
